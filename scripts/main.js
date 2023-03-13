28faff8a80981c79b47bd89b48ca98fa91eb300e
Abraham OLAGUNJU <36889745+OluwaninsolaAO@users.noreply.github.com>
2023-03-13 13:47:58 +0100

    Add comments

--- scripts/main.js
+++ scripts/main.js
@@ -1,9 +1,12 @@
+// Variables
 let greeting = document.querySelector('#greeting');
 let action = document.querySelector('#action');
 let forget = document.querySelector('#forget');
 
+// Onload -- the follwoing commands load with the page
 greeting.textContent = `Hello there ${localStorage.getItem('name') === null ? "Stranger" : localStorage.getItem('name') }!`;
 
+// Event listeners
 action.onclick = () => {
     let name = prompt(`Enter your name please.`);
     greetUser(name);
@@ -14,6 +17,7 @@ forget.onclick = () => {
     greetUser('Stranger');
 }
 
+// Handful functions
 function greetUser(name) {
     if (name === null || name === '')
         return;
