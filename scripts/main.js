3c6c4684e3a083988f58e041605e914e0a96144c
Abraham OLAGUNJU <36889745+OluwaninsolaAO@users.noreply.github.com>
2023-03-13 13:44:42 +0100

    Trying localStorage API

--- scripts/main.js
+++ scripts/main.js
@@ -1,10 +1,24 @@
 let greeting = document.querySelector('#greeting');
 let action = document.querySelector('#action');
+let forget = document.querySelector('#forget');
+
+greeting.textContent = `Hello there ${localStorage.getItem('name') === null ? "Stranger" : localStorage.getItem('name') }!`;
 
 action.onclick = () => {
     let name = prompt(`Enter your name please.`);
+    greetUser(name);
+}
 
-    while (!name)
-        name = prompt(`You must enter your name please.`);
-    greeting.textContent = `Hello there ${name}!`;
+forget.onclick = () => {
+    localStorage.removeItem('name');
+    greetUser('Stranger');
+}
+
+function greetUser(name) {
+    if (name === null || name === '')
+        return;
+    else
+        localStorage.setItem('name', name);
+    
+    greeting.textContent = `Hello there ${localStorage.getItem('name') === null ? "Stranger" : localStorage.getItem('name') }!`;
 }
