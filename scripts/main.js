de5458458f86bf5b08afce4b4311d53364ab5a6f
Abraham OLAGUNJU <36889745+OluwaninsolaAO@users.noreply.github.com>
2023-03-13 14:12:26 +0100

    Create a color switches with buttons

--- scripts/main.js
+++ scripts/main.js
@@ -3,6 +3,13 @@ let greeting = document.querySelector('#greeting');
 let action = document.querySelector('#action');
 let forget = document.querySelector('#forget');
 
+let need_color = document.querySelector('#need_color');
+let need_color_title = document.querySelector('#need_color_title');
+let primary = document.querySelector('#primary');
+let secondary = document.querySelector('#secondary');
+let warning = document.querySelector('#warning');
+let success = document.querySelector('#success');
+
 // Onload -- the follwoing commands load with the page
 greeting.textContent = `Hello there ${localStorage.getItem('name') === null ? "Stranger" : localStorage.getItem('name') }!`;
 
@@ -15,6 +22,23 @@ action.onclick = () => {
 forget.onclick = () => {
     localStorage.removeItem('name');
     greetUser('Stranger');
+}
+
+primary.onclick = () => {
+    need_color.setAttribute('class', 'bg-primary text-white');
+}
+
+secondary.onclick = () => {
+    need_color.setAttribute('class', 'bg-secondary text-white');
+}
+
+warning.onclick = () => {
+    need_color.setAttribute('class', 'bg-warning');
+}
+
+success.onclick = () => {
+    need_color.setAttribute('class', 'bg-success text-white');
+    need_color_title.textContent = "Ah! Success is better, Thank You! 😀";
 }
 
 // Handful functions
