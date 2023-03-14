19a550d89e8f160d491fa4c3025afc0ae2914f23
Abraham O. OLAGUNJU <36889745+OluwaninsolaAO@users.noreply.github.com>
2023-03-14 12:26:19 +0100

    Prettier

--- scripts/main.js
+++ scripts/main.js
@@ -1,57 +1,63 @@
 // Variables
-let greeting = document.querySelector('#greeting');
-let action = document.querySelector('#action');
-let forget = document.querySelector('#forget');
+let greeting = document.querySelector("#greeting");
+let action = document.querySelector("#action");
+let forget = document.querySelector("#forget");
 
-let need_color = document.querySelector('#need_color');
-let need_color_title = document.querySelector('#need_color_title');
-let primary = document.querySelector('#primary');
-let secondary = document.querySelector('#secondary');
-let warning = document.querySelector('#warning');
-let success = document.querySelector('#success');
+let need_color = document.querySelector("#need_color");
+let need_color_title = document.querySelector("#need_color_title");
+let primary = document.querySelector("#primary");
+let secondary = document.querySelector("#secondary");
+let warning = document.querySelector("#warning");
+let success = document.querySelector("#success");
 
 let need_color_default_title = need_color_title.textContent;
 
 // Onload -- the follwoing commands load with the page
-greeting.textContent = `Hello there ${localStorage.getItem('name') === null ? "Stranger" : localStorage.getItem('name') }!`;
+greeting.textContent = `Hello there ${
+  localStorage.getItem("name") === null
+    ? "Stranger"
+    : localStorage.getItem("name")
+}!`;
 
 // Event listeners
 action.onclick = () => {
-    let name = prompt(`Enter your name please.`);
-    greetUser(name);
-}
+  let name = prompt(`Enter your name please.`);
+  greetUser(name);
+};
 
 forget.onclick = () => {
-    localStorage.removeItem('name');
-    greetUser('Stranger');
-}
+  localStorage.removeItem("name");
+  greetUser("Stranger");
+};
 
 primary.onclick = () => {
-    need_color.setAttribute('class', 'bg-primary text-white');
-    need_color_title.textContent = need_color_default_title;
-}
+  need_color.setAttribute("class", "bg-primary text-white");
+  need_color_title.textContent = need_color_default_title;
+};
 
 secondary.onclick = () => {
-    need_color.setAttribute('class', 'bg-secondary text-white');
-    need_color_title.textContent = need_color_default_title;
-}
+  need_color.setAttribute("class", "bg-secondary text-white");
+  need_color_title.textContent = need_color_default_title;
+};
 
 warning.onclick = () => {
-    need_color.setAttribute('class', 'bg-warning');
-    need_color_title.textContent = need_color_default_title;
-}
+  need_color.setAttribute("class", "bg-warning");
+  need_color_title.textContent = need_color_default_title;
+};
 
 success.onclick = () => {
-    need_color.setAttribute('class', 'bg-success text-white');
-    need_color_title.textContent = "Ah! Success is better, Thank You! 😀";
-}
+  need_color.setAttribute("class", "bg-success text-white");
+  need_color_title.textContent = "Ah! Success is better, Thank You! 😀";
+};
 
 // Handful functions
 function greetUser(name) {
-    if (name === null || name === '')
-        return;
-    else
-        localStorage.setItem('name', name);
-    
-    greeting.textContent = `Hello there ${localStorage.getItem('name') === null ? "Stranger" : localStorage.getItem('name') }!`;
+  if (name === null || name === "") return;
+  else localStorage.setItem("name", name);
+
+  greeting.textContent = `Hello there ${
+    localStorage.getItem("name") === null
+      ? "Stranger"
+      : localStorage.getItem("name")
+  }!`;
 }
